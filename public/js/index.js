7a3b6b067d0a35f45d4933bae910e1b943bfabcf
Nicolás Batistoni <nicolasbatistoni@gmail.com>
2018-06-12 15:03:45 -0300

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    from: 'Jen',
-    text: 'Hey, this is Jen.'
-  });
 });
 
 socket.on('disconnect', function () {
