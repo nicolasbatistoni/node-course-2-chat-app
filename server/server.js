012be0d38d62d5c9d3192ed744ca0bfdc9b5b98e
Nicolás Batistoni <nicolasbatistoni@gmail.com>
2018-06-12 18:42:36 -0300

    Add css fot chat page

--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ io.on('connection', (socket) => {
       message.from,
       message.text
     ));
-    callback('This is from the server.');
+    callback();
   });
 
   socket.on('createLocationMessage', (coords) => {
