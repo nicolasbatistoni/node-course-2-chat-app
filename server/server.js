811cd3cf990d09e757f0bdf22c7484bc3e3fb150
Nicolás Batistoni <nicolasbatistoni@gmail.com>
2018-06-12 13:34:06 -0300

    Init commit

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const express = require('express');
+
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+var app = express();
+
+app.use(express.static(publicPath));
+
+app.listen(3000, () => {
+  console.log(`Server is up on port ${port}`);
+});
+
+// require('./config/config');
+//
+// const _ = require('lodash')
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const {ObjectID} = require('mongodb');
+//
+// var {mongoose} = require('./db/mongoose');
